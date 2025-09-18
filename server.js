2609ae54e45bab33b3b5873c46c70bbfbe759f9e
nhotinhdau <javidjdjjjj@gmail.com>
2025-09-18 17:33:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // --- CẤU HÌNH ---
-const HISTORY_API_URL = 'https://loq.onrender.com/api/lxk';
+const HISTORY_API_URL = 'https://kjjbv.onrender.com/api/lxk';
 let cachedConfidence = null;
 let cachedSession = null;
 
@@ -122,5 +122,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`🚀 Server đang chạy trên cổng ${PORT}`));
+
 
 
